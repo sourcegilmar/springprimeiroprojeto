cee2731ada02458eaddb631a817b3f0fff312847
sourcegilmar <gilmardw@gmail.com>
2022-10-28 15:00:24 -0300

    Implementação Use Entity Resource

---
+++ src/main/java/com/gilmarborba/curso/resource/UserResource.java
@@ -0,0 +1,20 @@
+package com.gilmarborba.curso.resource;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.gilmarborba.curso.entities.User;
+
+@RestController
+@RequestMapping(value = "/users")
+public class UserResource {
+
+	// endpoint para acessar os usuários
+	@GetMapping
+	public ResponseEntity<User>findAll() {
+		User u = new User(1L, "Maria", "maria@gmail.com", "60706070", "1234");
+		return ResponseEntity.ok().body(u);
+	}
+}
