3f468653a0f5116fb7333756724051f5d6c89430
sourcegilmar <gilmardw@gmail.com>
2022-11-07 15:00:48 -0300

    Delete User

--- src/main/java/com/gilmarborba/curso/resource/UserResource.java
+++ src/main/java/com/gilmarborba/curso/resource/UserResource.java
@@ -5,6 +5,7 @@ import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -54,5 +55,13 @@ public class UserResource {
 		return ResponseEntity.ok().body(obj);
 	}
 	
+	// Para deletar o método padrão HTTP que usamos é o
+	// delete. O código HTTP para uma requisição que não tem
+	// conteúdo é o 204 (que é o caso do delete)
+	@DeleteMapping(value = "/{id}")
+	public ResponseEntity<Void> delete(@PathVariable Long id) {
+		service.delete(id);
+		return ResponseEntity.noContent().build();
+	}	
 	
 }
--- src/main/java/com/gilmarborba/curso/services/UserService.java
+++ src/main/java/com/gilmarborba/curso/services/UserService.java
@@ -42,5 +42,9 @@ public class UserService {
 	}
 	
 	
+	public void delete(Long id) {
+		repository.deleteById(id);
+	}
+	
 	
 }
